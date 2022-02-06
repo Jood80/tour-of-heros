a3c11c0771b1dce8a8b35a1b65c8db87e39bf6c6
Jood80 <shawar.nujood@gmail.com>
2022-02-06 21:59:51 +0200

    feat: add<add & delete> features

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -23,4 +23,17 @@ export class HerosComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) return;
+    this.heroService
+      .addHero({ name } as Hero)
+      .subscribe((hero) => this.heroes.push(hero));
+  }
+
+  delete(RemovedHero: Hero): void {
+    this.heroes = this.heroes.filter((hero) => hero !== RemovedHero);
+    this.heroService.deleteHero(RemovedHero.id).subscribe();
+  }
 }
